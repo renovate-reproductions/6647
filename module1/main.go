7fa529bfa6960969c2d5d49ebcb54b63564500a4
Asato Wakisaka <asato.wakisaka@gmail.com>
2020-07-02 02:50:38 +0900

    add pkg/errors

--- module1/main.go
+++ module1/main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/astj/renovate-sandbox-go-mod-replace/module2"
+	"github.com/pkg/errors"
 )
 
 func main() {
 	fmt.Println(module2.Version)
+	fmt.Println(errors.New("ohno"))
 }
