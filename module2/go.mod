86630b9501dda63c77a4c00e8b3a6d978db253c7
Asato Wakisaka <asato.wakisaka@gmail.com>
2020-07-08 21:35:47 +0900

    add dependency to module2

--- module2/go.mod
+++ module2/go.mod
@@ -1,3 +1,5 @@
 module github.com/astj/renovate-sandbox-go-mod-replace/module2
 
 go 1.14
+
+require github.com/pkg/json v0.0.0-20200627043909-c7586d2f1f37
--- module2/version.go
+++ module2/version.go
@@ -1,4 +1,8 @@
 package module2
 
+import (
+	"github.com/pkg/json"
+)
+
 // Version is version
-var Version = "0.0.1"
+var Version = "0.0.1 " + string(json.True)
